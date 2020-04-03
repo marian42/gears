1b03984fde4a848533f9b27c25ffc2ef1ac67b09
Marian <mail@marian42.de>
2020-04-04 00:42:17 +0200

    Add gear decorations

--- script.js
+++ script.js
@@ -9,11 +9,58 @@ class GearSVG {
         this.pathStrings = [];
 
         this.radiusPitch = n / 2;
-        this.radiusInner = this.radiusPitch - 1.35;
+        this.radiusInner = this.radiusPitch - 1.2;
         this.radiusOuter = this.radiusPitch + 0.85;
         this.radiusIntermediate = this.radiusInner + 0.9;
 
         this.createTeeth();
+
+        var xExtension = 0;
+        var yExtension = 0;
+        var extensionSize = 0.5;
+
+        switch (n) {
+            case 20:
+                xExtension = 1.6;
+                yExtension = 1.6;
+                break;
+            case 36:
+                yExtension = 1.2;
+                this.addCircle(-8, 0);
+                this.addCircle(+8, 0);
+                this.createAxleHole(0, 8, 0, 1.2);
+                this.createAxleHole(0, -8, 0, 1.2);
+                break;
+            case 16:
+                this.addCircle(0, -4, 3.4);
+                this.addCircle(0, +4, 3.4);
+                this.addCircle(-4, 0, 3.4);
+                this.addCircle(+4, 0, 3.4);
+                break;
+            case 24:
+                yExtension = 3.8;
+                extensionSize = 1.6
+                this.addCircle(-4, -4);
+                this.addCircle(-4, +4);
+                this.addCircle(+4, +4);
+                this.addCircle(+4, -4);
+                break;
+            case 40:
+                yExtension = 2;
+                for (var x = -1; x < 2; x += 2) {
+                    for (var y = -1; y < 2; y += 2) {
+                        this.addCircle(x * 4, y * 4);
+                        this.addCircle(x * 12, y * 4);
+                        this.addCircle(x * 4, y * 12);
+                    }
+                }
+                this.createAxleHole(0, -8, 4.4, 0);
+                this.createAxleHole(0, +8, 4.4, 0);
+                this.createAxleHole(+8, 0, 0, 4.4);
+                this.createAxleHole(-8, 0, 0, 4.4);
+                break;
+        }
+        this.createAxleHole(0, 0, xExtension, yExtension, extensionSize);
     }
 
     createTeeth() {
@@ -41,6 +88,60 @@ class GearSVG {
         }
     }
 
+    addCircle(x, y, diameter=5) {
+        const r = diameter / 2;
+        this.pathStrings.push("M " + (x - r) + ", " + y);
+        this.pathStrings.push("a " + r + "," + r + " 0 1, 0 " + diameter + ",0");
+        this.pathStrings.push("a " + r + "," + r + " 0 1, 0 " + (-diameter) + ",0");
+    }
+
+    createAxleHole(x = 0, y = 0, xExtension=2, yExtension=0, extensionSize=0.5) {
+        const a = 1.78 / 2;
+        const b = 4.78 / 2;
+        const c = extensionSize / 2;
+        var vertices = [
+            [x - b, y - a],
+
+            [x - b, y - c],
+            [x - b - xExtension, y - c],
+            [x - b - xExtension, y + c],
+            [x - b, y + c],
+
+            [x - b, y + a],
+            [x - a, y + a],
+            [x - a, y + b],
+
+            [x - c, y + b],
+            [x - c, y + b + yExtension],
+            [x + c, y + b + yExtension],
+            [x + c, y + b],
+
+            [x + a, y + b],
+            [x + a, y + a],
+            [x + b, y + a],
+
+            [x + b, y + c],
+            [x + b + xExtension, y + c],
+            [x + b + xExtension, y - c],
+            [x + b, y - c],
+
+            [x + b, y - a],
+            [x + a, y - a],
+            [x + a, y - b],
+
+            [x + c, y - b],
+            [x + c, y - b - yExtension],
+            [x - c, y - b - yExtension],
+            [x - c, y - b],
+
+            [x - a, y - b],
+            [x - a, y - a],
+        ];
+
+        console.log(vertices.reverse());
+        this.addPolygon(vertices.reverse());
+    }
+
     getSVG() {
         const svgNamespace = "http://www.w3.org/2000/svg";
         var svg = document.createElementNS(svgNamespace, "svg");    
@@ -65,5 +166,11 @@ function createGear(n) {
 const gears = [8, 16, 24, 40, 12, 20, 36];
 
 for (var n of gears) {
+    document.body.appendChild(createGear(n));
+}
+
+document.body.appendChild(document.createElement("br"));
+
+for (var n = 10; n < 32; n++) {
     document.body.appendChild(createGear(n));
 }
