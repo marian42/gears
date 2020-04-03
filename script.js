3a6e7d80f23541fd9e2a86469253d67beb407fac
Marian <mail@marian42.de>
2020-04-03 23:44:26 +0200

    Create a class for gear SVGs

--- script.js
+++ script.js
@@ -3,44 +3,63 @@ function getOnCircle(angle, radius) {
     return [Math.cos(angle) * radius, Math.sin(angle) * radius];
 }
 
+class GearSVG {
+    constructor(n) {
+        this.teeth = n;
+        this.pathStrings = [];
+
+        this.radiusPitch = n / 2;
+        this.radiusInner = this.radiusPitch - 1.35;
+        this.radiusOuter = this.radiusPitch + 0.85;
+        this.radiusIntermediate = this.radiusInner + 0.9;
+
+        this.createTeeth();
+    }
+
+    createTeeth() {
+        var vertices = [];
+    
+        for (var i = 0; i < n; i++) {
+            var fraction = 2 * Math.PI / n;
+            var angle = i * fraction;
+    
+            vertices.push(getOnCircle(angle - fraction * 0.29, this.radiusInner));
+            vertices.push(getOnCircle(angle - fraction * 0.25, this.radiusIntermediate));
+            vertices.push(getOnCircle(angle - fraction * 0.11, this.radiusOuter));
+            vertices.push(getOnCircle(angle + fraction * 0.11, this.radiusOuter));
+            vertices.push(getOnCircle(angle + fraction * 0.25, this.radiusIntermediate));
+            vertices.push(getOnCircle(angle + fraction * 0.29, this.radiusInner));
+        }
+
+        this.addPolygon(vertices);
+    }
+
+    addPolygon(vertices) {    
+        this.pathStrings.push("M " + vertices[vertices.length - 1][0] + " " + vertices[vertices.length - 1][1]);
+        for (var vertex of vertices) {
+            this.pathStrings.push("L " + vertex[0] + " " + vertex[1]);
+        }
+    }
+
+    getSVG() {
+        const svgNamespace = "http://www.w3.org/2000/svg";
+        var svg = document.createElementNS(svgNamespace, "svg");    
+        var path = document.createElementNS(svgNamespace, "path");    
+    
+        path.setAttribute("d", this.pathStrings.join(' '));
+    
+        svg.appendChild(path);
+        svg.setAttribute("height", this.radiusOuter * 8);
+        svg.setAttribute("width", this.radiusOuter * 8);
+        svg.setAttribute("viewBox", (-this.radiusOuter) + " " + (-this.radiusOuter) + " " + (2 * this.radiusOuter) + " " + (2 * this.radiusOuter));
+        svg.setAttribute("class", "gear")
+    
+        return svg;
+    }
+}
+
 function createGear(n) {
-    const radiusPitch = n / 2;
-    const radiusInner = radiusPitch - 1.35;
-    const radiusOuter = radiusPitch + 0.85;
-    const radiusIntermediate = radiusInner + 0.9;
-
-    const svgNamespace = "http://www.w3.org/2000/svg";
-    var svg = document.createElementNS(svgNamespace, "svg");
-
-    var polygon = document.createElementNS(svgNamespace, "polygon");
-
-    var vertices = [];
-
-    for (var i = 0; i < n; i++) {
-        var fraction = 2 * Math.PI / n;
-        var angle = i * fraction;
-
-        vertices.push(getOnCircle(angle - fraction * 0.29, radiusInner));
-        vertices.push(getOnCircle(angle - fraction * 0.25, radiusIntermediate));
-        vertices.push(getOnCircle(angle - fraction * 0.11, radiusOuter));
-        vertices.push(getOnCircle(angle + fraction * 0.11, radiusOuter));
-        vertices.push(getOnCircle(angle + fraction * 0.25, radiusIntermediate));
-        vertices.push(getOnCircle(angle + fraction * 0.29, radiusInner));
-    }
-
-    var verticesStrings = [];
-    for (var vertex of vertices) {
-        verticesStrings.push(vertex[0] + "," + vertex[1]);
-    }
-    polygon.setAttribute("points", verticesStrings.join(' '));
-
-    svg.appendChild(polygon);
-    svg.setAttribute("height", radiusOuter * 10);
-    svg.setAttribute("width", radiusOuter * 10);
-    svg.setAttribute("viewBox", (-radiusOuter) + " " + (-radiusOuter) + " " + (2 * radiusOuter) + " " + (2 * radiusOuter));
-    svg.setAttribute("class", "gear")
-
-    return svg;
+    return new GearSVG(n).getSVG();
 }
 
 const gears = [8, 16, 24, 40, 12, 20, 36];
